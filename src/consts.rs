cd356a70fd7f442dc690688b630fb3f41ca19c5d
Denis Mukhametov <mukhametov.d@skbkontur.ru>
2023-05-01 04:19:13 +0600

    Add firestore

---
+++ src/consts.rs
@@ -0,0 +1,2 @@
+pub const TEXT_MESSAGES_COLLECTION: &str = "text_messages";
+pub const VIDEO_NOTES_COLLECTION: &str = "video_notes";
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use firestore::{errors::FirestoreError, FirestoreDb};
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use teloxide::{
     prelude::*,
     types::{InputFile, KeyboardButton, KeyboardMarkup, VideoNote},
     update_listeners::webhooks,
 };
+mod consts;
 
 fn make_keyboard() -> KeyboardMarkup {
     let button = KeyboardButton::new("Нажми меня!");
@@ -27,14 +31,46 @@ async fn send_video_note_back_with_file_id(
     Ok(())
 }
 
+async fn get_db(client: &Client) -> FirestoreDb {
+    let project_id = client
+        .get("http://metadata.google.internal/computeMetadata/v1/project/project-id")
+        .header("Metadata-Flavor", "Google")
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    FirestoreDb::new(project_id).await.unwrap()
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct TextMessage {
+    text: String,
+}
+
+async fn get_text_messages(db: &FirestoreDb) -> Result<Vec<TextMessage>, FirestoreError> {
+    Ok(db
+        .fluent()
+        .select()
+        .from(consts::TEXT_MESSAGES_COLLECTION)
+        .obj()
+        .query()
+        .await?)
+}
+
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
     log::info!("Starting false-nikita-bot...");
 
+    let client = reqwest::Client::new();
+
+    let db = get_db(&client).await;
     let bot = Bot::from_env();
 
-    let url = format!("https://false-nikita-bot-kliudduhya-lm.a.run.app/webhook")
+    let url = "https://false-nikita-bot-kliudduhya-lm.a.run.app/webhook"
         .parse()
         .unwrap();
 
@@ -46,15 +82,23 @@ async fn main() {
 
     teloxide::repl_with_listener(
         bot,
-        |bot: Bot, msg: Message| async move {
-            if let Some(video_note) = msg.video_note() {
-                send_video_note_back_with_file_id(&bot, &msg, video_note).await?;
-            } else {
-                bot.send_message(msg.chat.id, "pong")
-                    .reply_markup(make_keyboard())
-                    .await?;
+        move |bot: Bot, msg: Message| {
+            let db = db.clone();
+            async move {
+                if let Some(video_note) = msg.video_note() {
+                    send_video_note_back_with_file_id(&bot, &msg, video_note).await?;
+                } else {
+                    if let Ok(result) = get_text_messages(&db.clone()).await {
+                        bot.send_message(msg.chat.id, &result[0].text).await?;
+                    } else {
+                        bot.send_message(msg.chat.id, "Database request Error")
+                            .reply_markup(make_keyboard())
+                            .await?;
+                        return Ok(());
+                    }
+                }
+                Ok(())
             }
-            Ok(())
         },
         listener,
     )
