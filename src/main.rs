a7160b2a202c6cec811e27973f98c3d59c3869a5
Denis Mukhametov <mukhametov.d@skbkontur.ru>
2023-04-30 21:06:31 +0600

    Send video notes back with file id

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::net::SocketAddr;
 use teloxide::{
     prelude::*,
-    types::{KeyboardButton, KeyboardMarkup},
+    types::{InputFile, KeyboardButton, KeyboardMarkup, VideoNote},
     update_listeners::webhooks,
 };
 
@@ -12,6 +12,19 @@ fn make_keyboard() -> KeyboardMarkup {
         .resize_keyboard(true)
         .persistent()
         .input_field_placeholder(String::from("test"))
+}
+
+async fn send_video_note_back_with_file_id(
+    bot: &Bot,
+    message: &Message,
+    video_note: &VideoNote,
+) -> Result<(), teloxide::errors::RequestError> {
+    let id = &video_note.file.id;
+    let video_note = InputFile::file_id(id);
+    bot.send_message(message.chat.id, format!("Video note file id is {}", id))
+        .await?;
+    bot.send_video_note(message.chat.id, video_note).await?;
+    Ok(())
 }
 
 #[tokio::main]
@@ -34,9 +47,13 @@ async fn main() {
     teloxide::repl_with_listener(
         bot,
         |bot: Bot, msg: Message| async move {
-            bot.send_message(msg.chat.id, "pong")
-                .reply_markup(make_keyboard())
-                .await?;
+            if let Some(video_note) = msg.video_note() {
+                send_video_note_back_with_file_id(&bot, &msg, video_note).await?;
+            } else {
+                bot.send_message(msg.chat.id, "pong")
+                    .reply_markup(make_keyboard())
+                    .await?;
+            }
             Ok(())
         },
         listener,
