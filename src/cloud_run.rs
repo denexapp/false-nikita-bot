4900fd5da7dcd2500b69ac607f0cf2bbfb391f95
Denis Mukhametov <mukhametov.d@skbkontur.ru>
2023-05-02 06:23:34 +0600

    Add token to service uri request

--- src/cloud_run.rs
+++ src/cloud_run.rs
@@ -10,6 +10,11 @@ pub struct Service {
     pub uri: String,
 }
 
+#[derive(Deserialize)]
+pub struct Token {
+    pub access_token: String,
+}
+
 pub async fn get_project_id(client: &Client) -> String {
     client
         .get("http://metadata.google.internal/computeMetadata/v1/project/project-id")
@@ -41,6 +46,20 @@ async fn get_instance_region(client: &Client) -> String {
     .to_string()
 }
 
+async fn get_access_token(client: &Client) -> String {
+    let token: Token = client
+        .get("http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token")
+        .header("Metadata-Flavor", "Google")
+        .send()
+        .await
+        .expect("A request for token should not fail when sending")
+        .json()
+        .await
+        .expect("A request for token should not fail when awaiting for json and decoding");
+
+    token.access_token
+}
+
 fn get_service_name() -> String {
     env::var("K_SERVICE")
         .expect("K_SERVICE environment variable should have Cloud Run service name")
@@ -56,9 +75,11 @@ pub async fn get_service_uri(client: &Client, project_id: &str) -> String {
 
     let url = format!("https://run.googleapis.com/v2/projects/{encoded_project_id}/locations/{encoded_instance_region}/services/{encoded_service_name}");
 
+    let access_token = get_access_token(client).await;
+
     let service = client
         .get(&url)
-        .header("Metadata-Flavor", "Google")
+        .header("Authorization", format!("Bearer {access_token}"))
         .send()
         .await
         .expect("A request for Service information should not fail when sending")
@@ -72,7 +93,7 @@ pub async fn get_service_uri(client: &Client, project_id: &str) -> String {
     );
 
     let service: Service = client
-        .get(&url)
+        .get(url)
         .header("Metadata-Flavor", "Google")
         .send()
         .await
